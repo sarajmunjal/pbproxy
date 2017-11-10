1fa49c3f7ea29ab7bd1094971604f00bdeb748a8
Saraj Munjal <saraj.munjal@gmail.com>
2017-11-10 02:47:13 -0500

    Fixed single client mode

--- hw3/src/pbproxy.c
+++ hw3/src/pbproxy.c
@@ -39,40 +39,47 @@ int main(int argc, char **argv) {
     int reading = 1;
     int writing = 1;
     while (writing || reading) {
-        bzero(buffer, 1024);
-        n = recv(sockfd, buffer, 1023, NULL);
-        if (n < 0) {
-            error("ERROR reading from socket");
-            break;
-        }
-        if (n == 0) {
-            continue;
-        } else {
-            printf("%s\n", buffer);
-        }
-        bzero(buffer, 1024);
 
-        int read_in = read(STDIN_FILENO, buffer, 1023);
-        if (read_in < 0) {
-            error("Failed to read from stdin");
-            break;
+        // write_loop
+        while (1) {
+            bzero(buffer, 1024);
+            int read_in = read(STDIN_FILENO, buffer, 1023);
+            if (read_in < 0) {
+                error("Failed to read from stdin");
+                break;
+            }
+            if (read_in == 0) {
+                //
+                break;
+            }
+            buffer[read_in] = '\0';
+            n = write(sockfd, buffer, strlen(buffer));
+            if (n < 0) {
+                error("ERROR writing to socket");
+                break;
+            }
+            if (n == 0) {
+                // nothing to write
+                continue;
+            }
         }
-        if (read_in == 0) {
-            //
-            continue;
-        }
-        buffer[read_in] = '\0';
-        n = write(sockfd, buffer, strlen(buffer));
-        if (n < 0) {
-            error("ERROR writing to socket");
-            break;
-        }
-        if (n == 0) {
-            // nothing to write
-            continue;
+
+
+        bzero(buffer, 1024);
+        //read_loop
+        while (1) {
+            n = recv(sockfd, buffer, 1023, NULL);
+            if (n < 0) {
+                error("ERROR reading from socket");
+                break;
+            }
+            if (n == 0) {
+                break;
+            } else {
+                printf("%s\n", buffer);
+            }
         }
     }
     close(sockfd);
     return 0;
-
 }
